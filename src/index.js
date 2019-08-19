23e45a5370c6310132db7c85171bd2f761b964e7
Yusuke Miyaura <ichheisseyuhakase@gmail.com>
2019-08-19 12:29:12 +0900

    changed

--- src/index.js
+++ src/index.js
@@ -2,9 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import AppTwo from './AppTwo';
+import moment from 'moment-timezone';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+moment.tz.setDefault('Asia/Tokyo');
+moment().format('YYYY年MM月DD日');
+
+
+ReactDOM.render(
+  React.createElement(App, null, null),
+  document.getElementById('root-one')
+);
+
+ReactDOM.render(
+  React.createElement(AppTwo, null, null), document.getElementById('root-two')
+);
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
